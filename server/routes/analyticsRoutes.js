f9e80983f1ae976726fdc910687849adec2d3056
shahbazaman <shabushahbaz123@gmail.com>
2026-03-02 15:46:54 +0530

    fixed error

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getMonthlyReport } from '../controllers/analyticsController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
